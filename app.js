d0232e9feff744f601fb5f387a5eead8b16f8155
anfelo <af.osorio1341@gmail.com>
2017-07-22 11:27:43 -0500

    Init program files

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+const express = require('express');
+const jsonParser = require('body-parser').json;
+const app = express();
+
+app.use(jsonParser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// Error Handler
+app.use(function(err,req,res,next) {
+	res.status(err.status || 500);
+	res.json({
+		error: {
+			message: err.message
+		}
+	});
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+	console.log("Express server is listening on port", port);
+});
